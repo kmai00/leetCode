80d956dbd43ac662b7f4670d2ac4416288d220a2
Kevin Mai <kmai270@gmail.com>
2021-03-19 20:10:35 -0600

    First try, but the Min() breaks it

---
+++ LeetCode/Problem456.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetCode
+{
+    //Problem 456 - Medium
+    // 132 problem
+
+    //Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+    //Return true if there is a 132 pattern in nums, otherwise, return false.
+
+    public class Problem456
+    {
+        // Idea number one: Binary tree
+        // -- If I have a left and right child, that's makes a subsequence
+
+        public bool Find132Pattern(int[] nums)
+        {
+        }
+    }
+}
---
+++ LeetCodeTests/Problem456_Tests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class Problem456_Tests
+    {
+
+        Problem456 _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Problem456();
+        }
+
+        [Test]
+        public void OrderedArray()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+        }
+    }
+}
