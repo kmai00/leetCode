6b99b4ce8cfbb96f397a7dd300b38df6a656c503
Kevin Mai <kmai270@gmail.com>
2021-03-24 23:50:39 -0600

    FizzBuzz 80% faster and 18.65% less mem

---
+++ LeetCode/Problem412.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    // 412. Fizz Buzz
+    // Easy
+
+    // Write a program that outputs the string representation of numbers from 1 to n.
+    // But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+    public class Problem412
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            var result = new List<string>();
+            var phrase = string.Empty;
+            for (var i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    phrase = "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    phrase += "Buzz";
+                }
+
+                // This seems redundant;
+                if (string.IsNullOrEmpty(phrase))
+                {
+                    result.Add(i.ToString());
+                }
+                else
+                {
+                    result.Add(phrase);
+                }
+
+                phrase = string.Empty;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem412Tests.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem412Tests
+    {
+        Problem412 _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Problem412();
+        }
+
+        [Test]
+        public void Example_1()
+        {
+            var n = 15;
+            var expected = new List<string> {
+               "1",
+               "2",
+                "Fizz",
+                "4",
+                "Buzz",
+                "Fizz",
+                "7",
+                "8",
+                "Fizz",
+                "Buzz",
+                "11",
+                "Fizz",
+                "13",
+                "14",
+                "FizzBuzz"
+            };
+
+            var result = _sut.FizzBuzz(n);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
