59f193baeeab433f60e398832f2be3e91fe8d9f7
Kevin Mai <kmai270@gmail.com>
2021-03-28 22:15:58 -0600

    Solved, great memory but terrible performance

---
+++ LeetCode/Problem594.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCode
+{
+    // Problem 594. Longest Harmonious Subsequence
+    // Easy
+
+    //We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+
+    //Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+
+    //A subsequence of array is a sequence that can be derived from the array by deleting some or no elements without changing the order of the remaining elements.
+
+    public class Problem594
+    {
+        public int FindLHS(int[] nums)
+        {
+            // Let's try something dumb first...
+
+            var numberTracker = new HashSet<int>(); // Key and count
+            var biggestCount = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var smallCount = 0;
+                var bigCount = 0;
+                var isOnlyKey = true;
+                var key = nums[i];
+                if (numberTracker.Contains(key))
+                {
+                    continue; //skip cause we already counted you
+                }
+                else
+                {
+                    smallCount = 1;
+                    bigCount = 1;
+                }
+
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var candidate = nums[j];
+                    // Count for when I'm the smallest
+                    // Count for when I'm the biggest
+                    // Take the bigger Count
+
+                    if (candidate == key) // TODO need to count whenever it's not the only one in the list...
+                    {
+                        smallCount++;
+                        bigCount++;
+                    }
+                    else if (key + 1 == candidate)
+                    {
+                        bigCount++;
+                        isOnlyKey = false;
+                    }
+                    else if (key - 1 == candidate)
+                    {
+                        smallCount++;
+                        isOnlyKey = false;
+                    }
+
+                }
+
+                var higherCount = isOnlyKey ? 0 : Math.Max(smallCount, bigCount);
+
+                if (higherCount > biggestCount)
+                {
+                    biggestCount = higherCount;
+                }
+
+            }
+
+            return biggestCount;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem594Tests.cs
@@ -0,0 +1,69 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem594Tests
+    {
+        Problem594 _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Problem594();
+        }
+
+        [Test]
+        public void Example_1()
+        {
+            var nums = new int[] { 1, 3, 2, 2, 5, 2, 3, 7 };
+
+            var result = _sut.FindLHS(nums);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void Example_2()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+
+            var result = _sut.FindLHS(nums);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Example_3()
+        {
+            var nums = new int[] { 1, 1, 1, 1 };
+
+            var result = _sut.FindLHS(nums);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Example_4()
+        {
+            var nums = new int[] { 1, 2, 1, 3, 0, 0, 2, 2, 1, 3, 3 };
+            var result = _sut.FindLHS(nums);
+
+            Assert.AreEqual(6, result);
+        }
+
+        [Test]
+        public void Example_5()
+        {
+            var nums = new int[] { 1, 4, 1, 3, 1, -14, 1, -13 };
+            var result = _sut.FindLHS(nums);
+
+            Assert.AreEqual(2, result);
+        }
+
+    }
+}
