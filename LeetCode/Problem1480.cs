d80513a61d49b8bf9ecc6f5c0d43d90873492172
Kevin Mai <kmai270@gmail.com>
2023-06-23 09:16:19 -0600

    Solve problem 1480

---
+++ LeetCode/Problem1480.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Problem1480
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            var resultList = new int[nums.Length];
+            var currentResult = nums[0];
+            resultList[0] = currentResult;
+
+            for (var i = 1; i < nums.Length; i++) {
+                currentResult += nums[i];
+                resultList[i] = currentResult; 
+            }
+
+            return resultList;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem1480_Tests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem1480_Tests
+    {
+        private Problem1480 _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Problem1480();
+        }
+
+        [Test]
+        public void Example1()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+
+            var result = _sut.RunningSum(nums);
+            Assert.AreEqual(new int[] { 1, 3, 6, 10 }, result);
+        }
+    }
+}
