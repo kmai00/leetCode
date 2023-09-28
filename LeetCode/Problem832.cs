f63e742e32f0cc52840896650aac3840812e6f80
Kevin Mai <kmai270@gmail.com>
2023-09-28 15:18:52 -0600

    Solution for Problem832

---
+++ LeetCode/Problem832.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public static class Problem832
+    {
+        // Can use bitwise function to transform the value.
+
+        public static int[][] FlipAndInvertImage(int[][] image)
+        {
+            var result = new List<int[]>();
+            foreach (var img in image)
+            {
+                var mirroredImage = img.Reverse().ToArray();
+                var reversedValue = ReverseValue(mirroredImage);
+                result.Add(reversedValue);
+            }
+
+            return result.ToArray();
+        }
+
+        public static int[] ReverseValue(int[] img)
+        {
+            var result = new int[img.Length];
+            for (var i = 0; i < img.Length; i++)
+            {
+                result[i] = img[i] == 1 ? 0 : 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem832_Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem832_Tests
+    {
+        [Test]
+        public void Example1()
+        {
+            var image = new int[][] {
+            new int[] { 1, 1, 0 },
+            new int[] { 1, 0, 1 },
+            new int[] { 0, 0, 0 }
+        };
+            var result = Problem832.FlipAndInvertImage(image);
+
+            var expected = new int[][] {
+                new int[]{ 1,0,0 },
+                new int[]{ 0,1,0 },
+                new int[] { 1,1,1}
+            };
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
