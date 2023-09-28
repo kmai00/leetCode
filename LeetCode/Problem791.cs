fb059afc9497b2f376f9fbdd382905c307fd1c65
Kevin Mai <kmai270@gmail.com>
2023-09-28 14:55:51 -0600

    First iteration of Problem 791

---
+++ LeetCode/Problem791.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    public static class Problem791
+    {
+        public static string CustomSortString(string order, string s)
+        {
+            // I want to check out if the current order char is in s
+            // If so then add those until s went through
+            // If not and I meet the end of S, then move to the next ordered
+
+            var result = string.Empty;
+
+            var remainingChars = s.ToList();
+
+            foreach (var orderChar in order)
+            {
+                var i = 0;
+                while (i < remainingChars.Count)
+                {
+                    var potentialChar = remainingChars[i];
+                    if (orderChar == potentialChar)
+                    {
+                        result += potentialChar;
+                        remainingChars.RemoveAt(i);
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+
+            return result + string.Join(string.Empty, remainingChars);
+        }
+    }
+}
---
+++ LeetCodeTests/Problem791_Tests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem791_Tests
+    {
+
+        [Test]
+        public void Example1()
+        {
+
+            var input = "cba";
+            var s = "abcd";
+            var result = Problem791.CustomSortString(input, s);
+
+            Assert.AreEqual("cbad", result);
+        }
+    }
+}
