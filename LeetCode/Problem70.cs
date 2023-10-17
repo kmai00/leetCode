5626c2dd19872427960b8a7bcd0cc5d4cbb44f0e
Kevin Mai <kmai270@gmail.com>
2023-10-17 15:32:42 -0600

    Porblem70. Figuring out possibility of going down the steps

---
+++ LeetCode/Problem70.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Problem70
+    {
+        public int ClimbStairs(int n)
+        {
+            var answer = ClimbStairs(n, new Dictionary<int, int>());
+            return answer;
+        }
+
+        private int ClimbStairs(int target, Dictionary<int, int> memoized)
+        {
+            if (target == 0)
+            {
+                return 1;
+            }
+            else if (target < 0)
+            {
+                return 0; //Not possible
+            }
+            else if (memoized.ContainsKey(target)) { 
+                return memoized[target];
+            }
+
+            var possibleWays = ClimbStairs(target - 1, memoized) + ClimbStairs(target - 2, memoized);
+            memoized.Add(target, possibleWays);
+
+            return possibleWays;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem70.cs
@@ -0,0 +1,33 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem70Tests
+    {
+        Problem70 sut;
+
+        public Problem70Tests()
+        {
+            sut = new Problem70();
+        }
+
+        [Test]
+        public void Example1_Ok()
+        {
+            var result = sut.ClimbStairs(2);
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void Example2_Ok()
+        {
+            var result = sut.ClimbStairs(3);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
