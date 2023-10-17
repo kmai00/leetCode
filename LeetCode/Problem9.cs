de45a1536bb5de886f55449ed70074c677326adc
Kevin Mai <kmai270@gmail.com>
2023-10-17 11:57:51 -0600

    Problem9 reverse

---
+++ LeetCode/Problem9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Problem9
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false; // There will be a negative sign so it will not be true
+            }
+
+            var reverseDigits = new List<int>();
+
+            var value = x;
+            while (value > 0)
+            {
+                var digit = value % 10;
+                reverseDigits.Add(digit);
+                value = value / 10;
+            }
+
+            var orderedDigits = reverseDigits.ToList(); // Make a copy
+            orderedDigits.Reverse();
+
+            for (var i = 0; i < orderedDigits.Count; i++)
+            {
+                if (orderedDigits[i] != reverseDigits[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem9_Tests.cs
@@ -0,0 +1,40 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem9_Tests
+    {
+        private Problem9 sut;
+        public Problem9_Tests()
+        {
+            sut = new Problem9();
+        }
+
+
+        [Test]
+        public void Example1_Ok()
+        {
+            var result = sut.IsPalindrome(121);
+            Assert.IsTrue(result);
+        }
+        
+        [Test]
+        public void Example2_Ok()
+        {
+            var result = sut.IsPalindrome(-121);
+            Assert.IsFalse(result);
+        }
+        
+        [Test]
+        public void Example3_Ok()
+        {
+            var result = sut.IsPalindrome(10);
+            Assert.IsFalse(result);
+        }
+    }
+}
