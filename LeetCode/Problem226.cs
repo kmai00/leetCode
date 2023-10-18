357976f80085cc5a8bfa19f694aae0184c76a835
Kevin Mai <kmai270@gmail.com>
2023-10-18 17:50:04 -0600

    Reverse binary tree in BFS

---
+++ LeetCode/Problem226.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Problem226
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+
+            // Trying to invert with BFS
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode.left != null)
+                {
+                    queue.Enqueue(currentNode.left);
+                }
+                if (currentNode.right != null)
+                {
+                    queue.Enqueue(currentNode.right);
+                }
+
+                var temp = currentNode.left;
+                currentNode.left = currentNode.right;
+                currentNode.right = temp;
+            }
+
+            return root;
+        }
+    }
+}
