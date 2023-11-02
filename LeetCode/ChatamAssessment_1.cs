bff29653caed30785956d51a8ed36bbbb7167ec1
Kevin Mai <kmai270@gmail.com>
2023-11-02 17:22:24 -0600

    Chattam initial interview

---
+++ LeetCode/ChatamAssessment_1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Stats
+    {
+        // Total number of all words in the document
+        public int NumberOfAllWords { get; set; }
+
+        // Returns number of words that consist only from digits e.g. 13455, 98374
+        public int NumberOfWordsThatContainOnlyDigits { get; set; }
+
+        // Returns number of words that start with a lower letter e.g. a, d, z
+        public int NumberOfWordsStartingWithSmallLetter { get; set; }
+
+        // Returns number of words that start with a capital letter e.g. A, D, Z
+        public int NumberOfWordsStartingWithCapitalLetter { get; set; }
+
+        // Returns the first longest word in the document
+        public string TheLongestWord { get; set; }
+
+        // Returns the first shortest word in the document
+        public string TheShortestWord { get; set; }
+    }
+
+    public class ChatamAssessment_1
+    {
+        public Stats Analyze(string document)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            //I'm assuming no stats on empty documents
+            if (string.IsNullOrWhiteSpace(document))
+            {
+                return new Stats();
+            }
+
+            //There might be some bad data between each words which results in empty entries
+            var words = document.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var onlyDigitCount = 0;
+            var startLowercaseCount = 0;
+            var startUppercaseCount = 0;
+
+            var longestWord = string.Empty;
+            var shortestWord = string.Empty;
+            foreach (var word in words)
+            {
+
+                var isDigit = word.All(char.IsDigit);
+                if (isDigit)
+                {
+                    onlyDigitCount++;
+                }
+
+                var startingChar = word[0];
+                if (char.IsLower(startingChar))
+                {
+                    startLowercaseCount++;
+                }
+                //Explicit checking in case words start with symbols/digits
+                if (char.IsUpper(startingChar))
+                {
+                    startUppercaseCount++;
+                }
+
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+
+                if (word.Length < shortestWord.Length || string.IsNullOrEmpty(shortestWord))
+                {
+                    shortestWord = word;
+                }
+            };
+
+            return new Stats
+            {
+                NumberOfAllWords = words.Length,
+                NumberOfWordsThatContainOnlyDigits = onlyDigitCount,
+                NumberOfWordsStartingWithSmallLetter = startLowercaseCount,
+                NumberOfWordsStartingWithCapitalLetter = startUppercaseCount,
+                TheLongestWord = longestWord,
+                TheShortestWord = shortestWord,
+            };
+        }
+    }
+}
---
+++ LeetCodeTests/ChatamAssessment_1_Tests.cs
@@ -0,0 +1,84 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+
+    [TestFixture]
+    public class ChatamAssessment_1_Tests
+    {
+        private ChatamAssessment_1 sut;
+        
+        public ChatamAssessment_1_Tests()
+        {
+            sut = new ChatamAssessment_1();
+        }
+
+        [Test]
+        public void NumberOfWords_Ok()
+        {
+            var doc = "I'm a very good programmer. Even though I am just 12 years old.";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual(13, result.NumberOfAllWords);
+        }
+
+        [Test]
+        public void NumbersOfOnlyDigit_Ok()
+        {
+            var doc = "I'm a very good programmer. Even though I am just 12 years old.";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual(1, result.NumberOfWordsThatContainOnlyDigits);
+        }
+
+        [Test]
+        public void NumbersOfStartingLowercase_Ok()
+        {
+            var doc = "I'm a very good programmer. Even though I am just 12 years old.";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual(9, result.NumberOfWordsStartingWithSmallLetter);
+        }
+
+
+        [Test]
+        public void NumbersOfStartingUppercase_Ok()
+        {
+            var doc = "I'm a very good programmer. Even though I am just 12 years old.";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual(3, result.NumberOfWordsStartingWithCapitalLetter);
+        }
+
+        [Test]
+        public void LongestWord_Ok()
+        {
+            var doc = "I'm a very good programmer. Even though I am just 12 years old.";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual("programmer.", result.TheLongestWord);
+        }
+
+        [Test]
+        public void ShortestWord_Ok()
+        {
+            var doc = "I'm a very good programmer. Even though I am just 12 years old.";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual("a", result.TheShortestWord);
+        }
+
+        [Test]
+        public void TieForShortestAndLongest() {
+            var doc = "a b c xxx yyy zzz";
+            var result = sut.Analyze(doc);
+            Assert.AreEqual("a", result.TheShortestWord);
+            Assert.AreEqual("xxx", result.TheLongestWord);
+        }
+
+        [Test]
+        public void IndexOutOfBound()
+        {
+            var doc = ", A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67%^&%&^%67457687643576438756438756$%$%$%%4343 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff , A . 1 B 2 C a&^&&%%$##%%^&5656 3 a 4 b 5 c 6 &8&%%$67 jkkk sssff";
+            var result = sut.Analyze(doc);
+        }
+    }
+}
