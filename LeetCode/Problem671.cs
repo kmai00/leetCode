8900d99b3165e55eb41c6600a90667024b7d40af
Kevin Mai <kmai270@gmail.com>
2023-11-02 18:15:00 -0600

    Second smallest

---
+++ LeetCode/Problem671.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+
+    //Second smallest number
+
+    public class SmallNumbers
+    {
+        public int smallestNumber;
+        public int CandidateForSecondSmallest;
+    }
+
+    public class Problem671
+    {
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var smallNumbers = new SmallNumbers
+            {
+                smallestNumber = root.val,
+                CandidateForSecondSmallest = -1
+            };
+
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode.left != null)
+                {
+                    queue.Enqueue(currentNode.left);
+                }
+                if (currentNode.right != null)
+                {
+                    queue.Enqueue(currentNode.right);
+                }
+
+                var currentVal = currentNode.val;
+                if (smallNumbers.smallestNumber == currentVal)
+                {
+                    continue;
+                }
+
+                //Establish upper bound
+                var currentValue = currentNode.val;
+                if (smallNumbers.CandidateForSecondSmallest == -1) { 
+                    smallNumbers.CandidateForSecondSmallest = currentValue;
+                }
+
+                // If we have a value that's smaller than upper bound, but bigger than the smallest- it's now the second
+                if (smallNumbers.CandidateForSecondSmallest > currentVal && smallNumbers.smallestNumber < currentVal)
+                {
+                    smallNumbers.CandidateForSecondSmallest = currentVal;
+                }
+                else if (smallNumbers.smallestNumber > currentVal) {
+                    var secondSmallest = smallNumbers.smallestNumber;
+                    smallNumbers.smallestNumber = currentVal;
+                    smallNumbers.CandidateForSecondSmallest = secondSmallest;
+                }
+
+            }
+
+            return smallNumbers.CandidateForSecondSmallest;
+        }
+    }
+}
---
+++ LeetCodeTests/Problem671_Tests.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class Problem671_Tests
+    {
+        [Test]
+        public void Example_1()
+        {
+            var tree = new TreeNode
+            {
+                val = 2,
+                left = new TreeNode
+                {
+                    val = 2
+                },
+                right = new TreeNode
+                {
+                    val = 5,
+                    left = new TreeNode
+                    {
+                        val = 5
+                    },
+                    right = new TreeNode
+                    {
+                        val = 7
+                    }
+
+                }
+            };
+
+            var _sut = new Problem671();
+            var result = _sut.FindSecondMinimumValue(tree);
+            Assert.AreEqual(5, result);
+
+        }
+    }
+}
